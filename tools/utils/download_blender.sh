c973854b38b19b32859cd30d1939438d5bcacf53
Nutti <nutti.metro@gmail.com>
2019-04-14 22:33:46 +0900

    WIP

--- tools/utils/download_blender.sh
+++ tools/utils/download_blender.sh
@@ -9,7 +9,7 @@ SUPPORTED_VERSIONS=(
     "all"
 )
 
-declare -A BLENDER_DOWNLOAD_URL=(
+declare -A BLENDER_DOWNLOAD_URL_MACOSX=(
     ["v278"]="https://download.blender.org/release/Blender2.78/blender-2.78-OSX_10.6-x86_64.zip"
     ["v278a"]="https://download.blender.org/release/Blender2.78/blender-2.78a-OSX_10.6-x86_64.zip"
     ["v278b"]="https://download.blender.org/release/Blender2.78/blender-2.78b-OSX_10.6-x86_64-fixed.zip"
@@ -17,15 +17,80 @@ declare -A BLENDER_DOWNLOAD_URL=(
     ["v279"]="https://download.blender.org/release/Blender2.79/blender-2.79-macOS-10.6.tar.gz"
     ["v279a"]="https://download.blender.org/release/Blender2.79/blender-2.79a-macOS-10.6.zip"
     ["v279b"]="https://download.blender.org/release/Blender2.79/blender-2.79b-macOS-10.6.zip"
-    ["v280"]="https://builder.blender.org/download/blender-2.80-7c438e5366b2-OSX-10.9-x86_64.zip"
+    ["v280"]="https://builder.blender.org/download/blender-2.80-14884cda1ff5-OSX-10.9-x86_64.zip"
 )
 
-declare -A NEED_MOVE=(
+declare -A BLENDER_DOWNLOAD_URL_WIN64=(
+    ["v278"]="https://download.blender.org/release/Blender2.78/blender-2.78-windows64.zip"
+    ["v278a"]="https://download.blender.org/release/Blender2.78/blender-2.78a-windows64.zip"
+    ["v278b"]="https://download.blender.org/release/Blender2.78/blender-2.78b-windows64.zip"
+    ["v278c"]="https://download.blender.org/release/Blender2.78/blender-2.78c-windows64.zip"
+    ["v279"]="https://download.blender.org/release/Blender2.79/blender-2.79-windows64.zip"
+    ["v279a"]="https://download.blender.org/release/Blender2.79/blender-2.79a-windows64.zip"
+    ["v279b"]="https://download.blender.org/release/Blender2.79/blender-2.79b-windows64.zip"
+    ["v280"]="https://builder.blender.org/download/blender-2.80-14884cda1ff5-win64.zip"
+)
+
+declare -A NEED_MOVE_MACOSX=(
     ["v278c"]="blender-2.78c-OSX_10.6-x86_64"
     ["v279"]="blender-2.79-macOS-10.6"
     ["v279a"]="blender-2.79a-macOS-10.6"
     ["v279b"]="blender-2.79b-macOS-10.6"
 )
+
+declare -A NEED_MOVE_WIN64=(
+)
+
+function download_blender() {
+    ver=${1}
+    blender_download_url=${2}
+    move_from=${3}
+
+    # download Blender binary
+    cd ${current_dir}
+    target=blender-${ver}-bin
+    mkdir -p ${output_dir}/${target}
+    cd ${output_dir}/${target}
+
+    url=${blender_download_url}
+    filename=${target}
+    if [ ${url##*.} = "zip" ]; then
+        filename=${filename}.zip
+        curl ${url} -o ${filename}
+        unzip ${filename}
+        rm ${filename}
+    elif [ ${url##*.} = "gz" ]; then
+        filename=${filename}.gz
+        curl ${url} -o ${filename}
+        tar xvfz ${filename}
+        rm ${filename}
+    fi
+
+    if [ ! ${move_from} = "" ]; then
+        mv ${move_from}/* .
+    fi
+}
+
+function wait_for_all() {
+    local status=0
+    for pid in $@; do
+        wait ${pid} &&:
+        (exit $?) && (exit ${status}) &&:; status=$?
+    done
+    [ ${status} -ne 0 ] && exit ${status}
+}
+
+function check_os() {
+    if [ "$(uname)" == "Darwin" ]; then
+        echo "Mac"
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        echo "Linux"
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+        echo "Cygwin64"
+    else
+        echo "Unknown"
+    fi
+}
 
 # check arguments
 if [ $# -ne 2 ]; then
@@ -36,6 +101,10 @@ fi
 version=${1}
 current_dir=`pwd`
 output_dir=${2}
+
+# check operating system
+os=`check_os`
+echo "Operating System is ${os}"
 
 # check if the specified version is supported
 supported=0
@@ -50,50 +119,54 @@ if [ ${supported} -eq 0 ]; then
     exit 1
 fi
 
-function download_blender() {
-    ver=${1}
-
-    # download Blender binary
-    cd ${current_dir}
-    target=blender-${ver}-bin
-    mkdir -p ${output_dir}/${target}
-    cd ${output_dir}/${target}
-
-    url=${BLENDER_DOWNLOAD_URL[${ver}]}
-    filename=${target}
-    if [ ${url##*.} = "zip" ]; then
-        filename=${filename}.zip
-        curl ${url} -o ${filename}
-        unzip ${filename}
-    elif [ ${url##*.} = "gz" ]; then
-        filename=${filename}.gz
-        curl ${url} -o ${filename}
-        tar xvfz ${filename}
-    fi
-
-    if [[ "${NEED_MOVE[${ver}]+_}" == "_" ]]; then
-        mv ${NEED_MOVE[$ver]}/* .
-    fi
-}
-
-function wait_for_all() {
-    local status=0
-    for pid in $@; do
-        wait ${pid} &&:
-        (exit $?) && (exit ${status}) &&:; status=$?
-    done
-    [ ${status} -ne 0 ] && exit ${status}
-}
-
 if [ ${version} = "all" ]; then
     pids=()
-    for KEY in ${!BLENDER_DOWNLOAD_URL[@]}; do
-        download_blender ${KEY} &
-        pids+=($!)
-    done
+    if [ ${os} == "Mac" ]; then
+        for KEY in ${!BLENDER_DOWNLOAD_URL_MACOSX[@]}; do
+            url=${BLENDER_DOWNLOAD_URL_MACOSX[${KEY}]}
+            move_from=""
+            if [[ "${NEED_MOVE_MACOSX[${KEY}]+_}" == "_" ]]; then
+                move_from=${NEED_MOVE_MACOSX[${KEY}]}
+            fi
+            download_blender ${KEY} ${url} ${move_from} &
+            pids+=($!)
+        done
+    elif [ ${os} == "Cygwin64" ]; then
+        for KEY in ${!BLENDER_DOWNLOAD_URL_WIN64[@]}; do
+            url=${BLENDER_DOWNLOAD_URL_WIN64[${KEY}]}
+            move_from=""
+            if [[ "${NEED_MOVE_WIN64[${KEY}]+_}" == "_" ]]; then
+                move_from=${NEED_MOVE_WIN64[${KEY}]}
+            fi
+            download_blender ${KEY} ${url} ${move_from} &
+            pids+=($!)
+        done
+    else
+        echo "Not supported operating system (OS=${os})"
+        exit 1
+    fi
     wait_for_all ${pids[@]}
 else
-    download_blender v${version%.*}${version##*.}
+    if [ ${os} == "Mac" ]; then
+        ver=v${version%.*}${version##*.}
+        url=${BLENDER_DOWNLOAD_URL_MACOSX[${ver}]}
+        move_from=""
+        if [[ "${NEED_MOVE_MACOSX[${ver}]+_}" == "_" ]]; then
+            move_from=${NEED_MOVE_MACOSX[${ver}]}
+        fi
+        download_blender ${ver} ${url} ${move_from}
+    elif [ ${os} == "Cygwin64" ]; then
+        ver=v${version%.*}${version##*.}
+        url=${BLENDER_DOWNLOAD_URL_WIN64[${ver}]}
+        move_from=""
+        if [[ "${NEED_MOVE_WIN64[${ver}]+_}" == "_" ]]; then
+            move_from=${NEED_MOVE_WIN64[${ver}]}
+        fi
+        download_blender ${ver} ${url} ${move_from}
+    else
+        echo "Not supported operating system (OS=${os})"
+        exit 1
+    fi
 fi
 
 cd ${current_dir}
