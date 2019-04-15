daa816b071692d5af5eaed1c152ed4b7f83c5fd0
Nutti <nutti.metro@gmail.com>
2019-04-15 20:27:38 +0900

    WIP

--- tools/pip_package/build_pip_package.sh
+++ tools/pip_package/build_pip_package.sh
@@ -89,6 +89,7 @@ if [ ${target} = "release" ]; then
     python setup.py bdist_wheel
 
     # copy the generated package to releaes directory
+    tar cvfz dddddddddddddddd
     cp -r dist ${release_dir}/
 
     # clean up
